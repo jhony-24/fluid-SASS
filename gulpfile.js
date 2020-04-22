d8be3ed91021e3ec1ef1282a3bd5363d4c3c2c0f
jhony <jhonyvega24@gmail.com>
2020-04-22 16:29:57 -0500

    gulpfile configurate

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, task, watch, series } = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require('gulp-autoprefixer');
+
+function sassTask() {
+    return (
+        src("./sass/main.scss")
+            .pipe(sass({
+                outputStyle: "compressed"
+            }))
+            .pipe(autoprefixer())
+            .pipe(dest("./css"))
+    );
+}
+
+function defaultTask() {
+    watch("sass/*.scss", series("sass"))
+}
+
+task("sass", sassTask);
+task("default", defaultTask);
+
