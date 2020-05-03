5a55aa484667e96ae0168c00af0dd1f4ff92855e
jhony <jhonyvega24@gmail.com>
2020-05-03 13:00:38 -0500

    add new link 'examples'

--- docs/assets/js/data.js
+++ docs/assets/js/data.js
@@ -2,11 +2,12 @@ var typesAnimation = [
     "bounce", "bounce-inset", "bounce-rotate",
     "fade", "fade-intermittent", "fade-left", "fade-right",
     "from-left", "from-right", "from-top", "from-bottom",
-    "shake", "shake-rotate", "shake-vertical", "shake-cross"
+    "shake", "shake-rotate", "shake-vertical", "shake-cross",
 ];
 
 var links = [
     { to: "#home", text: "home" },
     { to: "#mixins", text: "mixins and class" },
+    { to: "#examples", text: "examples", },
     { to: "#downloads", text: "downloads" },
 ];
