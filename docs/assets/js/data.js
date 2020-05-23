bea8e6b6a0e5b60e1d1e3585bacdb87fcb682d80
jhony <jhonyvega24@gmail.com>
2020-05-23 18:55:44 -0500

    add types animation of type bounce in the data script in the doc

--- docs/assets/js/data.js
+++ docs/assets/js/data.js
@@ -1,5 +1,5 @@
 var typesAnimation = {
-    bounce: ["bounce", "bounce-inset", "bounce-rotate",],
+    bounce: ["bounce", "bounce-inset", "bounce-rotate","bounce-expand-x","bounce-expand-y"],
     fade: ["fade", "fade-intermittent", "fade-left", "fade-right", "fade-rotate",],
     from: ["from-left", "from-right", "from-top", "from-bottom",],
     shake: ["shake", "shake-rotate", "shake-vertical", "shake-cross",],
